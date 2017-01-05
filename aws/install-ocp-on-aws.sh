cb1ba121c9242300219458177df1403d97ef8aff
Didier Wojciechowski <dwojciec@redhat.com>
2017-01-05 14:56:52 +0100

    correct GITHUB env

--- aws/install-ocp-on-aws.sh
+++ aws/install-ocp-on-aws.sh
@@ -1,4 +1,26 @@
 cd /home/vagrant/AWS/openshift-ansible-contrib/reference-architecture/aws-ansible
+echo "./ose-on-aws.py  -v \
+        --public-hosted-zone=$DOMAIN \
+        --keypair=OSE-key \
+        --rhsm-user=$SUB_USERNAME \
+        --rhsm-password=$SUB_PASSWORD \
+        --rhsm-pool=$SUB_POOLID \
+        --master-instance-type=m4.large \
+        --node-instance-type=m4.xlarge \
+        --app-instance-type=m4.xlarge \
+        --region=$REGION \
+        --ami=$AMI \
+        --create-key=yes \
+        --key-path=/root/.ssh/id_rsa.pub \
+        --github-client-secret=$GITHUB_CLIENT_SECRET \
+        --github-organization=$GITHUB_ORGANIZATION \
+        --github-client-id=$GITHUB_CLIENT_ID"
+
+read -p "Are you sure you wish to continue? (yes/no)"
+if [ "$REPLY" != "yes" ]; then
+   exit
+fi
+echo "deploying OCP on AWS..."
 ./ose-on-aws.py  -v \
 	--public-hosted-zone=$DOMAIN \
 	--keypair=OSE-key \
@@ -12,6 +34,6 @@ cd /home/vagrant/AWS/openshift-ansible-contrib/reference-architecture/aws-ansibl
 	--ami=$AMI \
 	--create-key=yes \
 	--key-path=/root/.ssh/id_rsa.pub \
-	--github-client-secret=$GITHUB-CLIENT-SECRET \
-	--github-organization=$GITHUB-ORGANIZATION \
-        --github-client-id=$GITHUB-CLIENT-ID
+	--github-client-secret=$GITHUB_CLIENT_SECRET \
+	--github-organization=$GITHUB_ORGANIZATION \
+        --github-client-id=$GITHUB_CLIENT_ID

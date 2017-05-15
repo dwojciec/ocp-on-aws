cee23116a71a44ced8b68f6b638891c5e1121c10
dwojciec <dwojciec@redhat.com>
2017-05-15 12:39:24 +0200

    3.5

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 sudo -i 
-#echo "disable all repositories and enable only the required ones..."
-#subscription-manager repos --disable="*" > /dev/null
+echo "disable all repositories and enable only the required ones..."
+#subscription-manager register --auto-attach --force
+subscription-manager repos --disable="*" > /dev/null
+subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-server-extras-rpms" \
+    --enable="rhel-7-server-ose-3.5-rpms" \
+    --enable="rhel-7-fast-datapath-rpms"
 subscription-manager repos --enable rhel-7-server-optional-rpms
-subscription-manager repos --enable rhel-7-server-ose-3.5-rpms
-subscription-manager repos --enable rhel-7-fast-datapath-rpms
+subscription-manager refresh 
+echo " yum repolist"
+yum repolist
+echo "/etc/yum.conf "
+cat /etc/yum.conf 
+echo "install epel"
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 echo "install atomic-openshift-utils,python2-boto,...."
 yum -y install ansible atomic-openshift-utils
